d05a6e09c3b2eae8cfedf681a2d01261e401e4e0
Jay Vana <jaysvana@gmail.com>
2026-01-21 09:56:29 -0800

    feat: add StatCategoryType enum for stat drilldown routing

---
+++ FullDuplex/Models/StatCategoryType.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+enum StatCategoryType: String, CaseIterable, Identifiable {
+    case entities
+    case grids
+    case bands
+    case modes
+    case parks
+
+    var id: String { rawValue }
+
+    var title: String {
+        switch self {
+        case .entities: return "Entities"
+        case .grids: return "Grids"
+        case .bands: return "Bands"
+        case .modes: return "Modes"
+        case .parks: return "Parks"
+        }
+    }
+
+    var icon: String {
+        switch self {
+        case .entities: return "globe"
+        case .grids: return "square.grid.3x3"
+        case .bands: return "waveform"
+        case .modes: return "dot.radiowaves.right"
+        case .parks: return "leaf"
+        }
+    }
+}
